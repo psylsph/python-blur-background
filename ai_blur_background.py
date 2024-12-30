2b5d66d81cd698575314ebdc65422bbeb02bd882
Stuart Harding <psylsph@gmail.com>
2024-12-30 14:55:57 +0000

    support HEIC

--- ai_blur_background.py
+++ ai_blur_background.py
@@ -1,6 +1,9 @@
 import cv2
 import tempfile
-from PIL import Image, ImageFilter
+from wand.image import Image as WandImage
+from PIL import Image as PILImage
+from PIL import ImageFilter
+import numpy
 import os
 import requests
 from os.path import isfile
@@ -17,7 +20,7 @@ def download_model(url):
         file.write(response.content)
     print(f"Downloaded file {MODEL_PATH}")
 
-def remove_background_from_image(image: Image):
+def remove_background_from_image(image: PILImage):
     """Removes background from the image."""
     with tempfile.TemporaryDirectory() as tmp:
         source_image = os.path.join(tmp, 'source_image.png')
@@ -25,10 +28,10 @@ def remove_background_from_image(image: Image):
         image.save(source_image, format='PNG')
         extracted_img, mask = remove_background(MODEL_PATH, source_image)
         cv2.imwrite(bg_removed_image, extracted_img)
-        output_image = Image.open(bg_removed_image)
+        output_image = PILImage.open(bg_removed_image)
     return output_image
     
-def blur_image(image: Image, blur_amount):
+def blur_image(image: PILImage, blur_amount):
     """Blurs the image."""
     return image.filter(ImageFilter.GaussianBlur(radius=blur_amount))
 
@@ -37,7 +40,7 @@ def combine_images(foreground_image, background_image):
     background_image.paste(foreground_image, (0, 0), foreground_image)
     return background_image
 
-def image_to_base64(image: Image):
+def image_to_base64(image: PILImage):
     buffered = io.BytesIO()
     image.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
@@ -45,28 +48,30 @@ def image_to_base64(image: Image):
 
 
 st.set_page_config(layout="wide")
-
+st.markdown("#### AI Blur Background")
 if not isfile(MODEL_PATH):
     with st.spinner("Downloading model..."):
         download_model("https://huggingface.co/stoned0651/isnet_dis.onnx/resolve/main/isnet_dis.onnx")
         st.write("Model downloaded.")
 
 uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "heic"])
-blur_amount = st.sidebar.slider("Blur Amount", 0, 20, 10)
+blur_amount = st.sidebar.slider("Blur Amount", 0, 50, 25)
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
-    st.write("")
-    st.write("Removing background...")
-    background_removed = remove_background_from_image(image.copy())
-    st.image(background_removed, caption="Background Removed", use_container_width=True)
-    st.write("")
-    st.write("Blurring background...")
-    blurred_image = blur_image(image.copy(), blur_amount)
-    st.image(blurred_image, caption="Blurred Image", use_container_width=True)
-    st.write("")
-    st.write("Combining images...")
-    combined_image = combine_images(background_removed, blurred_image)
-    st.image(combined_image, caption="Combined Image", use_container_width=True)
-    st.write("")
-    st.write("Done!")
+    with WandImage( filename=uploaded_file.name) as wand_image:
+        img_buffer = numpy.asarray(bytearray(wand_image.make_blob(format='png')), dtype='uint8')
+        bytesio = io.BytesIO(img_buffer)
+        image = PILImage.open(bytesio)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
+        st.write("")
+        st.write("Removing background...")
+        background_removed = remove_background_from_image(image.copy())
+        st.image(background_removed, caption="Background Removed", use_container_width=True)
+        st.write("")
+        blurred_image = blur_image(image.copy(), blur_amount)
+        st.write("Combining images...")
+        combined_image = combine_images(background_removed, blurred_image)
+        st.image(combined_image, caption="Combined Image", use_container_width=True)
+        st.write("")
+        combined_image.convert('RGB').save('combined_image.jpeg')
+        with open('combined_image.jpeg', 'rb') as f:
+            st.download_button('Download File', f, file_name='combined_image.jpeg', mime='image/jpeg')
